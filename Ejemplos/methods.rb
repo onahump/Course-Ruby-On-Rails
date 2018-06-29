2e12d41f6c0233e7c7883605f4eecd7a2e36d884
onahump <oswaldo@makingdevs.com>
2018-06-28 21:42:53 -0500

    Agregando metodos de multiplicacio y division

--- Ejemplos/methods.rb
+++ Ejemplos/methods.rb
@@ -2,8 +2,37 @@ def saludar(nombre,saludo)
 	puts "Hola #{nombre}, que tengas #{saludo}"
 end
 
-print "Introduce tu nombre: "
+def suma(num1, num2)
+	suma = num1 + num2
+	return suma
+end	
 
-nombre = gets.chomp
+def resta(num1, num2)
+	resta = num1 - num2
+	return resta
+end	
 
-saludar(nombre, "buenas tardes")
+def multiplicacion(num1, num2)
+	multiplicacion = num1 * num2
+	return multiplicacion
+end	
+
+def division(num1, num2)
+	division = num1 / num2
+	return division
+end	
+
+a = 21
+b = 4
+
+resultado = suma(a, b)
+puts "La suma de #{a} + #{b} es #{resultado}"
+
+resultado = resta(a, b)
+puts "La resta de #{a} - #{b} es #{resultado}"
+
+resultado = multiplicacion(a, b)
+puts "La multiplicacion de #{a} * #{b} es #{resultado}"
+
+resultado = division(a, b)
+puts "La division de #{a} / #{b} es #{resultado}"
