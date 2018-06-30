d33690c1d7ff9f830f13638d86b0402e55d0b38d
onahump <oswaldo@makingdevs.com>
2018-06-30 18:17:34 -0500

    Creando ejemplo de hashes con ejemplos

---
+++ Ejemplos/contactos.rb
@@ -0,0 +1,24 @@
+contactos = {nahum:5553538402,
+			 raul: 5529085061}
+
+p contactos
+
+p"*"*100
+
+contactos[:pedro] = 5567845676
+
+p contactos
+
+p"*"*100
+
+contactos.merge({ perla: 5567492090,
+							  lalo: 5563525671})
+
+p contactos
+
+p"*"*100
+
+contactos = contactos.merge({ perla: 5567492090,
+							  lalo: 5563525671})
+
+p contactos
