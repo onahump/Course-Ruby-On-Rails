ea6520baf46bd742cc362f99b2d5c4cc6e9eb07a
onahump <oswaldo@makingdevs.com>
2018-06-30 18:21:37 -0500

    Agregando comentarios al ejemplo de hashes

--- Ejemplos/contactos.rb
+++ Ejemplos/contactos.rb
@@ -1,3 +1,5 @@
+#Creando hash de contactos
+
 contactos = {nahum:5553538402,
 			 raul: 5529085061}
 
@@ -5,18 +7,22 @@ p contactos
 
 p"*"*100
 
+# Agregando un contacto al hash de contactos 
 contactos[:pedro] = 5567845676
 
 p contactos
 
 p"*"*100
 
-contactos.merge({ perla: 5567492090,
-							  lalo: 5563525671})
+# Utilizando metodo merge para combinar dos hashes sin modificar al original 
+p contactos.merge({ perla: 5567492090,
+				  lalo: 5563525671})
 
 p contactos
 
 p"*"*100
+
+# Igualando el metodo merge con la variable original de contactos para agregar los nuevos contactos
 
 contactos = contactos.merge({ perla: 5567492090,
 							  lalo: 5563525671})
