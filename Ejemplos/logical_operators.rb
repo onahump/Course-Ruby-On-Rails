3a46b76605d797a773f612f83c339aaaaec56ca0
onahump <oswaldo@makingdevs.com>
2018-06-29 20:37:38 -0500

    Mejorando ejercicio de operadores logicos

--- Ejemplos/logical_operators.rb
+++ Ejemplos/logical_operators.rb
@@ -1,16 +1,25 @@
-calificacion = 10 
+def evaluar(calificacion)
+	
+	if calificacion == 0 || calificacion == 1 
+		print "Te pasas de penitente, no estudiaste ni maiz!!"
+	elsif calificacion > 1 && calificacion < 7 
+		print "Reprobaste canijo"
+	elsif calificacion == 7 || calificacion == 8
+		print "Panzaste"
+	elsif calificacion == 9
+		print "Aprobaste"
+	elsif calificacion == 10
+		print "Felicidades, eres un shingon"
+	else
+		print "La calificacion no es valida"
+	end
+end
 
-if calificacion == 0 || calificacion == 1 
-	print "Te pasas de penitente, no estudiaste ni maiz!!"
-elsif calificacion > 1 && calificacion < 7 
-	print "Reprobaste canijo"
-elsif calificacion == 7 || calificacion == 8
-	print "Panzaste"
-elsif calificacion == 9
-	print "Aprobaste"
-else 
-	print "Felicidades, eres un shingon"
-end 
+print "Dime tu calificacion: "
+calificacion = gets.chomp.to_i
+
+
+evaluar(calificacion)
 	
 	
 	
