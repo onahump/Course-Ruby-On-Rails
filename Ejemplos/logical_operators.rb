d7a4884e13239a4c149043e61ac71596e97b7426
onahump <oswaldo@makingdevs.com>
2018-06-29 20:32:29 -0500

    Creando y agregando ejemplo de operadores logicos

---
+++ Ejemplos/logical_operators.rb
@@ -0,0 +1,17 @@
+calificacion = 10 
+
+if calificacion == 0 || calificacion == 1 
+	print "Te pasas de penitente, no estudiaste ni maiz!!"
+elsif calificacion > 1 && calificacion < 7 
+	print "Reprobaste canijo"
+elsif calificacion == 7 || calificacion == 8
+	print "Panzaste"
+elsif calificacion == 9
+	print "Aprobaste"
+else 
+	print "Felicidades, eres un shingon"
+end 
+	
+	
+	
+	
