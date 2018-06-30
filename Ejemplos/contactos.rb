d7aba659ab287342a207445dbee3c8aefb0abac4
onahump <oswaldo@makingdevs.com>
2018-06-30 18:34:16 -0500

    Agregando mas ejemplos de metodos de hashes

--- Ejemplos/contactos.rb
+++ Ejemplos/contactos.rb
@@ -28,3 +28,30 @@ contactos = contactos.merge({ perla: 5567492090,
 							  lalo: 5563525671})
 
 p contactos
+
+# Regresar lista con todas las llaves de 
+
+p"*"*100
+
+lista_con_nombre_de_contactos = contactos.keys
+
+p lista_con_nombre_de_contactos
+
+# Regresar lista con todos los valores
+
+p"*"*100
+
+valores_de_la_lista_de_contactos = contactos.values
+
+p valores_de_la_lista_de_contactos
+
+# Buscar una llave en especifico 
+p"*"*100
+
+p contactos.has_key?(:aldo)
+
+# Agregar un contacto 
+p"*"*100
+contactos.store(:perengano, 67895465)
+
+p contactos
