dfde134576ead507ac7680f52b8c6ee4415b7906
onahump <oswaldo@makingdevs.com>
2018-07-01 19:02:16 -0500

    Agregando comentario

--- Clases/Perro.rb
+++ Clases/Perro.rb
@@ -11,6 +11,8 @@ class Perro
 		puts "#{ladrido} #{ladrido} #{ladrido}"
 	end
 
+
+	#Sirve para pasar mostrar nuestro objeto como una cadena en vez de ser mostrado como una direccion de memoria
 	def to_s
 		"Soy de raza #{@raza}, de color #{@color} y tengo #{@edad}"
 	end
