ce8b250401534bda35c4df5d972b7b40072ccf54
onahump <oswaldo@makingdevs.com>
2018-07-04 21:21:35 -0500

    Agregando espacios a la forma de mostrar la lista

--- Shopping_project/list.rb
+++ Shopping_project/list.rb
@@ -1,3 +1,5 @@
+require "./item.rb"
+
 class List
 
 	attr_writer :items
@@ -7,8 +9,30 @@ class List
 	end
 
 	def add_item(text)
-		item = Item.new
+		item = Item.new(text)
 		@items.push(item)
+	end
+
+	def remove_item(index)
+		@items.delete_at(index)
+	end
+
+	def checked_item(index)
+		@items[index].checked = true 
+	end
+
+	def remove_all
+		@items.clear
+	end
+
+	def show_all
+		if @items.length == 0 
+			puts "No hay articulos en tu lista de compras"
+		else
+			@items.each_index do |index|
+			puts index.to_s + " - " + @items[index].to_s
+			end
+		end 
 	end
 
 end
