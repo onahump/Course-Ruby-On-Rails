56e3d7a194f3d32f38593b5c48298f15609c8ac1
onahump <oswaldo@makingdevs.com>
2018-07-05 20:53:16 -0500

    Agregando un bloque a nuestro metodo por medio del campo yield

--- bloques/block.rb
+++ bloques/block.rb
@@ -1,5 +1,10 @@
 def hola
+	yield
+	puts "hola desde nuestra funcion"
+	yield #Saltar al bloque que hayamos asignado
 	puts "hola desde nuestra funcion"
 end
 
-hola
+hola do
+	puts "Hola desde nuestro bloque" #Pasamos nuesto bloqe
+end
