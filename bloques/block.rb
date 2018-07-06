67e711d32b7307150a14c73f1702950bb5a27d80
onahump <oswaldo@makingdevs.com>
2018-07-05 21:06:40 -0500

    Verificando si nos mandaron un bloque

--- bloques/block.rb
+++ bloques/block.rb
@@ -1,10 +1,14 @@
 def suma(num1, num2, &block)
 	puts "hola desde nuestra funcion"
 	resultado = num1 + num2
-	block.call resultado #Saltar al bloque que hayamos asignado
+	if block_given?
+		block.call resultado #Saltar al bloque que hayamos asignado
+	else
+		puts "El resultado desplegado en el metodo es #{resultado}"
+	end
 end
 
 suma(5,6) do |resultado|
-	puts "El resultado de nuestra operacion es #{resultado}" #Pasamos nuesto bloqe
+	puts "El resultado desplegado en el bloque es #{resultado}" #Pasamos nuesto bloqe
 end
 
