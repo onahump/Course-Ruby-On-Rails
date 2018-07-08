e10e57a1d57e46aff90d39faa8767c7af54bde0e
onahump <oswaldo@makingdevs.com>
2018-07-07 21:42:25 -0500

    Creando el metodo que nos da la descripcion del workshop y asignandolo a la variable workshop_description

--- Sinatra_app/app.rb
+++ Sinatra_app/app.rb
@@ -1,6 +1,12 @@
 require "sinatra"
 require "sinatra/reloader"
 
+def workshop_description(workshop_name)
+	File.read("workshops/#{workshop_name}.txt")
+rescue Errno::ENOENT #En caso de que exista un error regresara nulo
+	return nil
+end
+
 
 get '/' do
 	@files = Dir.entries("workshops")
@@ -9,6 +15,7 @@ end
 
 get '/:workshop_name' do
 	@workshop_name = params[:workshop_name]
+	@workshop_description = workshop_description(@workshop_name)
 	erb :workshop
 end
 
